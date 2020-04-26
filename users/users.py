9eebd9c320ef0209ed492c22a412d2ad2d646dc1
varshaahuja <va322@njit.edu>
2020-04-26 03:00:04 -0400

    Feature- Added Volunteering History, User Profile

--- users/users.py
+++ users/users.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from flask_mail import Message
-from app import mail_settings
+from app import mail_settings, db
+from auth.models import User, UserOrgJobs, OrgJobs
 
 user = Blueprint('user', __name__, template_folder='templates')
 
@@ -21,13 +22,24 @@ def editUser():
 @user.route('/userProfile')
 @login_required
 def userProfile():
-    return render_template('userProfile.html')
-
+    users = User.query.filter_by(id=current_user.id).first()
+    print(id)
+    return render_template('userProfile.html', users=users)
 
 
 @user.route('/volunteeringHistory')
 @login_required
 def volunteeringHistory():
+    ojid = UserOrgJobs.query.filter_by(id=current_user.id).first()
+    print(ojid)
+    orgjobs = db.session.query(OrgJobs).filter(OrgJobs.orgJob_id == ojid.orgJob_id)
+    print(orgjobs)
+    return render_template('volunteeringHistory.html', orgjobs=orgjobs)
+
+
+@user.route('/opportunities')
+@login_required
+def opportunities():
     return redirect(url_for('main.viewOpportunities'))
 
 
