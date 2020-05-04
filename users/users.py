ea9b7e31145212b98ed5dab251b5a9568765f2c0
varshaahuja <va322@njit.edu>
2020-05-04 17:35:55 -0400

    Fix- Updated the contact code msg details

--- users/users.py
+++ users/users.py
@@ -19,19 +19,20 @@ def editUser(id):
 
     user1 = User.query.filter_by(name=username).first()  # if this returns a user, then the username already exists in
     # database
-    if user1 and user1.name!=current_user.name :  # if a user is found, we want to redirect back to signup page so user can try again
+    if user1 and user1.name != current_user.name:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Sorry! Username already exists')
         return redirect(url_for('user.userProfile'))
 
     email1 = User.query.filter_by(email=email).first()  # if this returns a user, then the email already exists in
     # database
-    if email1 and email1.email!=current_user.email:  # if a user is found, we want to redirect back to signup page so user can try again
+    if email1 and email1.email != current_user.email:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Sorry! Email already exists')
         return redirect(url_for('user.userProfile'))
 
-    phone = User.query.filter_by(phone_number=phone_number).first()  # if this returns a user, then the email already exists in
+    phone = User.query.filter_by(
+        phone_number=phone_number).first()  # if this returns a user, then the email already exists in
     # database
-    if phone and phone.phone_number!=current_user.phone_number:  # if a user is found, we want to redirect back to signup page so user can try again
+    if phone and phone.phone_number != current_user.phone_number:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Sorry! Phone Number already exists')
         return redirect(url_for('user.userProfile'))
 
@@ -53,7 +54,6 @@ def editUser(id):
 @user.route('/deleteUser/<id>')
 @login_required
 def deleteUser(id):
-
     if current_user.is_admin():
         user = User.query.filter_by(id=id).delete()
         db.session.commit()
@@ -62,8 +62,6 @@ def deleteUser(id):
         user = User.query.filter_by(id=id).delete()
         db.session.commit()
         return redirect(url_for('auth.logout'))
-
-
 
 
 @user.route('/userProfile')
@@ -77,8 +75,8 @@ def userProfile():
 @user.route('/volunteeringHistory')
 @login_required
 def volunteeringHistory():
-    #orgjobs = db.session.query(OrgJobs, UserOrgJobs).filter(OrgJobs.orgJob_id == UserOrgJobs.orgJob_id,
-     #                                                       UserOrgJobs.id == current_user.id)
+    # orgjobs = db.session.query(OrgJobs, UserOrgJobs).filter(OrgJobs.orgJob_id == UserOrgJobs.orgJob_id,
+    #                                                       UserOrgJobs.id == current_user.id)
     orgjobs = db.session.query(UserOrgJobs).filter(UserOrgJobs.id == current_user.id)
     return render_template('volunteeringHistory.html', orgjobs=orgjobs)
 
@@ -93,25 +91,26 @@ def opportunities():
 def contact():
     return render_template('contact.html')
 
+
 @user.route('/mailAdmin', methods=['POST'])
 def mailAdmin_post():
-
     email = request.form.get('email')
     name = request.form.get('name')
     comments = request.form.get('comments')
 
-    user = User.query.filter_by(email=email).first()
+    #user = User.query.filter_by(email=email).first()
     msg = Message(subject="User Query",
                   sender=email,
                   recipients=[mail_settings["MAIL_USERNAME"]]  # replace with your email for testing
                   )
-    msg.html = '''<p>Hello</p> 
-    <p>You or someone else has requested that a new password be generated for your 
-    account.If you did not make this request, then you can simply ignore this email.</p> 
+    msg.html = '''<p>Hello Admin, </p> 
+    <p> A new user query has been raised by {}. </p>
+    <p> Below are the query details: </p>
+    <p> {} </p>
 
-    <p>Best Wishes,</p>
-    <p>Admin</p>
-    <p>Admin@volunteerrealm.com</p>'''
+    <p>User Details: </p>
+    <p> {} </p>
+    <p> {} </p>'''.format(name, comments, name, email)
     mail.send(msg)
     flash('Your message has been sent to the admin.')
     return redirect(url_for('user.contact'))
